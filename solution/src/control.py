b3caeda40335332198c03825fd67dbaf543c8ba4
Ian De La Garza <iandelagg@gmail.com>
2022-05-05 21:04:27 -0500

    changes

--- solution/src/control.py
+++ solution/src/control.py
@@ -18,24 +18,27 @@ class Control:
     #The message is going to be a Twist class objects
     msg = Twist()
 
-    dt = 0.01087
+    dt = 0.0108
     #Sintonization variables
+    #linear PID
     pid_l = PID(0.3, 0.001, 0.02, setpoint = 0, sample_time=dt) 
-    linearPub = rospy.Publisher('linearPID', PidState, queue_size = 10)
-    linear = PidState()
+    #linearPub = rospy.Publisher('linearPID', PidState, queue_size = 10)
+    #linear = PidState()
+    #angular PID
     pid_a = PID(2, 0.02, 0.02, setpoint = 0, sample_time=dt)
-    angularPub = rospy.Publisher('angularPID', PidState, queue_size = 10)
-    angular = PidState()
+    #angularPub = rospy.Publisher('angularPID', PidState, queue_size = 10)
+    #angular = PidState()
+
     def __init__(self):
         self.tf_buffer = Buffer()
         #Start tf listener
         self.listener = TransformListener(self.tf_buffer)
-        self.linear.p_term = self.pid_l.Kp
-        self.linear.i_term = self.pid_l.Ki
-        self.linear.d_term = self.pid_l.Kd
-        self.angular.p_term = self.pid_a.Kp
-        self.angular.i_term = self.pid_a.Ki
-        self.angular.d_term = self.pid_a.Kd
+        #self.linear.p_term = self.pid_l.Kp
+        #self.linear.i_term = self.pid_l.Ki
+        #self.linear.d_term = self.pid_l.Kd
+        #self.angular.p_term = self.pid_a.Kp
+        #self.angular.i_term = self.pid_a.Ki
+        #self.angular.d_term = self.pid_a.Kd
 
 
 
@@ -44,6 +47,7 @@ class Control:
     def setDesiredPosition(self, data):
         #Send the position using the broadcast method
         self.broadcastTransform(data)
+	#r = rospy.Rate(60)
         x=True
         while(x):
             try:
@@ -54,6 +58,7 @@ class Control:
                 x = (not self.startControl())
             except:
                 None
+	    #r.sleep()
         #Set velocities to 0
         self.setVelocities(Twist())
 
@@ -77,34 +82,35 @@ class Control:
     def pidCalculation(self, error):
         #Compute and Set linear and angular speed on msg
         self.msg.linear.x = -self.pid_l(error.x)
-        self.linear.header.stamp = rospy.Time.now()
-        self.linear.header.frame_id = "robot"
-        linearComp = self.pid_l.components
-        self.linear.error = error.x
-        self.linear.p_error = linearComp[0]
-        self.linear.i_error = linearComp[1]
-        self.linear.d_error = linearComp[2]
-        self.linear.output = self.msg.linear.x
-        self.linearPub.publish(self.linear)
+        #self.linear.header.stamp = rospy.Time.now()
+        #self.linear.header.frame_id = "robot"
+        #linearComp = self.pid_l.components
+        #self.linear.error = error.x
+        #self.linear.p_error = linearComp[0]
+        #self.linear.i_error = linearComp[1]
+        #self.linear.d_error = linearComp[2]
+        #self.linear.output = self.msg.linear.x
+        #self.linearPub.publish(self.linear)
 
         yaw = atan2(error.y,error.x)
         self.msg.angular.z = -self.pid_a(yaw)
-        self.angular.header.stamp = rospy.Time.now()
-        self.angular.header.frame_id = "robot"
-        angularComp = self.pid_a.components
-        self.angular.error = yaw
-        self.angular.p_error = angularComp[0]
-        self.angular.i_error = angularComp[1]
-        self.angular.d_error = angularComp[2]
-        self.angular.output = self.msg.angular.z
-        self.angularPub.publish(self.angular)
+        #self.angular.header.stamp = rospy.Time.now()
+        #self.angular.header.frame_id = "robot"
+        #angularComp = self.pid_a.components
+        #self.angular.error = yaw
+        #self.angular.p_error = angularComp[0]
+        #self.angular.i_error = angularComp[1]
+        #self.angular.d_error = angularComp[2]
+        #self.angular.output = self.msg.angular.z
+        #self.angularPub.publish(self.angular)
 
 
 
-        rospy.loginfo("pid_l: ", linearComp)
-        rospy.loginfo("pid_t:", angularComp)
+        #rospy.loginfo("pid_l: ", linearComp)
+        #rospy.loginfo("pid_t:", angularComp)
         #Publish velocities
         self.setVelocities(self.msg)
+        #self.posPub.publish(self.msg)
 
         
     #Publish the message for control propourses
